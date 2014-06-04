ccf262f5751845f3d14e7578d06d71fe230d0cb2
idiljay <ijama26@gmail.com>
2014-06-04 13:18:04 -0700

    hw assignment

---
+++ idiljama/quiz.js
@@ -0,0 +1,8 @@
+var userName = prompt("Whats your name?");
+document.write("<h2>" + userName + "</h2>");
+var numFriends = prompt("How many friends?");
+var numSlices = prompt("How many slices of pizza would you like?");
+var totalSlices = numFriends + numSlices;
+document.write("You should order: " + totalSlices);
+document.write("<img src='http://2.imimg.com/data2/HM/AO/MY-320173/pizza-box-250x250.jpg'>");
+
